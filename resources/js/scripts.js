07ac16204052c081deab4818f4e6b546ba05af9a
tungnguyenatp51 <79630159+tungnguyenatp51@users.noreply.github.com>
2021-03-01 13:19:45 +0700

    git commit -m "Ghi chú Commit"

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -14,11 +14,25 @@ $(document).ready(
         }
         )
         // scroll
-        $('a').click(function(event){
+        $('a').click(function (event) {
             $('html, body').animate({
-                scrollTop: $( $.attr(this, 'href') ).offset().top
+                scrollTop: $($.attr(this, 'href')).offset().top
             }, 700);
             event.preventDefault();
         });
+        // mobile navigation
+        $('.mobile-nav-icon').click(
+            function () {
+                $('.main-nav').slideToggle(200);
+                if ($('.mobile-nav-icon').hasClass('fa-bars')) {
+                    $('.mobile-nav-icon').addClass('fa-times');
+                    $('.mobile-nav-icon').removeClass('fa-bars')
+                }
+                else {
+                    $('.mobile-nav-icon').addClass('fa-bars');
+                    $('.mobile-nav-icon').removeClass('fa-times')
+                }
+            }
+        )       
     }
 )
